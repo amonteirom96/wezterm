679044d5b979236e897a7b67017a9ab23d0db412
arthurmonteiro <amonteirom96@gmail.com>
2025-08-05 20:20:31 -0300

    Change to fira code

--- wezterm.lua
+++ wezterm.lua
@@ -10,8 +10,8 @@ local theme = is_dark and core_theme.main or core_theme.dawn
 
 
 return {
-  font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" }),
-  font_size = 13.0,
+  font = wezterm.font("FiraCode Nerd Font", { weight = "Regular" }),
+  font_size = 15.0,
   colors = theme.colors(),
   window_frame = theme.window_frame(), -- needed only if using fancy tab bar
 
